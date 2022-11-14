77e7dbf7016d0598728ec9255d0b493b4907296f
firuzkhodjaev <firuzkhodjaev1111@gmail.com>
2022-11-14 15:05:37 +0500

    created elements_test / TextBox added elements_page.py, elements_page_locators.py, elements_page.py, generator.py, data.py

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+
+    # from fields
+
+    FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
+    EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='permanentAddress']")
+    SUBMIT = (By.XPATH, "//button[@id='submit']")
+
+    # created from
+    CREATED_FULL_NAME = (By.CSS_SELECTOR, '#output #name')
+    CREATED_EMAIL = (By.CSS_SELECTOR, '#output #email')
+    CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, '#output #currentAddress')
+    CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, '#output #permanentAddress')
