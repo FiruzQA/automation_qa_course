9fe3829b8deb481dcef9b60dd546cbd095502339
Firuz Khodjaev <firuzkhodjaev1111@gmail.com>
2022-11-21 17:28:35 +0500

    update elements test added test radio button

--- Pages/elements_page.py
+++ Pages/elements_page.py
@@ -7,6 +7,7 @@ from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from Pages.base_page import BasePage
 from locators.elements_page_locators import CheckBoxPageLocators
+from locators.elements_page_locators import RadioButtonPageLocators
 
 
 class TextBoxPage(BasePage):
@@ -66,3 +67,17 @@ class CheckBoxPage(BasePage):
         for item in result_list:
             data.append(item.text)
         return str(data).replace(' ', '').lower()
+
+
+class RadioButtonPage(BasePage):
+    locators = RadioButtonPageLocators()
+
+    def click_on_the_radio_button(self, choice):
+        choices = {'yes': self.locators.YES_RADIOBUTTON,
+                   'impressive': self.locators.IMPRESSIVE_RADIOBUTTON,
+                   'no': self.locators.NO_RADIOBUTTON}
+
+        self.element_is_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -2,6 +2,7 @@ import time
 
 from Pages.elements_page import CheckBoxPage
 from Pages.elements_page import TextBoxPage
+from Pages.elements_page import RadioButtonPage
 
 
 class TestElements:
@@ -27,4 +28,18 @@ class TestElements:
             output_result = check_box_page.get_output_result()
             assert input_checkbox == output_result, 'checkboxes have not been selected'
 
+    class TestRadioButton:
+
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
+            radio_button_page.open()
+            radio_button_page.click_on_the_radio_button('yes')
+            output_yes = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('impressive')
+            output_impressive = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('no')
+            output_no = radio_button_page.get_output_result()
+            assert output_yes == 'Yes', "'Yes' have not been selected"
+            assert output_impressive == 'Impressive', "'Impressive' have not been selected"
+            assert output_no == 'No', "'No' have not been selected"
 
