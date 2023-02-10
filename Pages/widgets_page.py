95524157e9bcec7790c2409d75c7a44559e54ee5
Firuz Khodjaev <firuzkhodjaev1111@gmail.com>
2023-02-10 15:31:03 +0500

    added class: TestWidgets added tests: - test_accordian

---
+++ Pages/widgets_page.py
@@ -0,0 +1,29 @@
+from selenium.common import TimeoutException
+
+from Pages.base_page import BasePage
+from locators.widgets_page_locators import AccordianPageLocators
+
+
+class AccordianPage(BasePage):
+    locators = AccordianPageLocators()
+
+    def check_accordian(self, accordian_num):
+        accordian = {'first':
+                         {'title': self.locators.SECTION_FIRST,
+                          'content': self.locators.SECTION_CONTENT_FIRST},
+                     'second':
+                         {'title': self.locators.SECTION_SECOND,
+                          'content': self.locators.SECTION_CONTENT_SECOND},
+                     'third':
+                         {'title': self.locators.SECTION_THIRD,
+                          'content': self.locators.SECTION_CONTENT_THIRD},
+                     }
+
+        section_title = self.element_is_visible(accordian[accordian_num]['title'])
+        section_title.click()
+        try:
+            section_content = self.element_is_visible(accordian[accordian_num]['content']).text
+        except TimeoutException:
+            section_title.click()
+            section_content = self.element_is_visible(accordian[accordian_num]['content']).text
+        return [section_title.text, len(section_content)]
